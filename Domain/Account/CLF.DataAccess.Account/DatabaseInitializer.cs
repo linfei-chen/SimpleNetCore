7202583d6f06e1a7d4dfe139072a5cfa9f95c9d9
Administrator <Administrator@DESKTOP-SI1FQK8>
2019-09-18 09:16:58 +0800

    提交所有解决方案

---
+++ Domain/Account/CLF.DataAccess.Account/DatabaseInitializer.cs
@@ -0,0 +1,22 @@
+﻿using CLF.Common.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CLF.DataAccess.Account
+{
+  public static  class DatabaseInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider = null)
+        {
+            DbContextOptions<AccountContext> options = null;
+            if (serviceProvider != null)
+                options = serviceProvider.GetService<DbContextOptions<AccountContext>>();
+            else
+                options = EngineContext.Current.Resolve<DbContextOptions<AccountContext>>();
+
+            var accountContext = new AccountContext(options);
+            accountContext.Database.Migrate();
+        }
+    }
+}
