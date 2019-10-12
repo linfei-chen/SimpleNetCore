146eed75137a4f4830e1c193a41d71a4ff764ab0
Administrator <Administrator@DESKTOP-SI1FQK8>
2019-10-12 16:45:20 +0800

    更新webapi接口

---
+++ WebApi/CLF.WebApi/Controllers/Account/PermissionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CLF.Service.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLF.WebApi.Controllers.Account
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionController : Controller
+    {
+        private readonly IAccountService _accountService;
+        public PermissionController(IAccountService accountService)
+        {
+            this._accountService = accountService;
+        }
+
+        /// <summary>
+        /// 查询权限列表
+        /// </summary>
+        /// <param name="start">起始页</param>
+        /// <param name="length">每页记录条数</param>
+        /// <param name="controllerName">控制器</param>
+        /// <param name="actionName">actionName</param>
+        /// <returns>权限列表</returns>
+        [HttpGet]
+        public ActionResult GetPermissions(string controllerName, string actionName, int start = 0, int length = 10)
+        {
+            var result = _accountService.FindPagenatedListWithCount(start, length, controllerName, actionName);
+            return Json(result);
+        }
+    }
+}
